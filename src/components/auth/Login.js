08a8a4383268d92b06227ec10349a44cbc106190
Brandon Herbison <herbisonbrandon@gmail.com>
2023-01-06 12:49:27 -0500

    login logo import bug fix

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,7 @@
 import { useRef, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { loginUser } from "../../managers/AuthManager"
-import BrainBulb from "/Users/brandonherbison/workspace/kwizzed/src/designs/BrainBulb.png"
+import BrainBulb from "../../designs/BrainBulb.png"
 
 
 export const Login = ({ setToken }) => {

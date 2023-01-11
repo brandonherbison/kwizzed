98935b3592e3289ae8d587422038133615b40bdf
Brandon Herbison <herbisonbrandon@gmail.com>
2023-01-11 16:18:27 -0500

    added content type

--- src/managers/AuthManager.js
+++ src/managers/AuthManager.js
@@ -1,6 +1,9 @@
 export const loginUser = (user) => {
   return fetch("http://localhost:8000/login", {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify({
       username: user.username,
       password: user.password
@@ -11,6 +14,9 @@ export const loginUser = (user) => {
 export const registerUser = (newUser) => {
   return fetch("http://localhost:8000/register", {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
 
     body: JSON.stringify(newUser)
   }).then(res => res.json())
